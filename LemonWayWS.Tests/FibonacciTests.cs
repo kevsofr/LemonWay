9d2549ca6d0f86093ca5f44be770b4e0e50b32ef
samielgtari <35293987+samielgtari@users.noreply.github.com>
2018-01-10 12:59:13 +0100

    Add Test

---
+++ LemonWayWS.Tests/FibonacciTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LemonWayWS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonWayWS.Tests
+{
+    [TestClass()]
+    public class FibonacciTests
+    {
+        [TestMethod()]
+        public void FibonacciTest()
+        {
+            LemonWayWS ws = new LemonWayWS();
+            Assert.AreEqual("1", ws.Fibonacci(1, false));
+            Assert.AreEqual("1", ws.Fibonacci(2, false));
+            Assert.AreEqual("2", ws.Fibonacci(3, false));
+            Assert.AreEqual("3", ws.Fibonacci(4, false));
+            Assert.AreEqual("5", ws.Fibonacci(5, false));
+            Assert.AreEqual("8", ws.Fibonacci(6, false));
+            Assert.AreEqual("354224848179261915075", ws.Fibonacci(100, false));
+            Assert.AreEqual("-1", ws.Fibonacci(-101, false));
+            Assert.AreEqual("-1", ws.Fibonacci(1000, false));
+        }
+
+
+    }
+}
---
+++ LemonWayWS.Tests/XMLtoJSONTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+
+namespace LemonWayWS.Tests
+{
+    [TestClass()]
+    public class XMLtoJSONTests
+    {
+        private LemonWayWS _ws = new LemonWayWS();
+        [TestMethod()]
+        public void XmlToJsonTest1()
+        {
+            string _in = @"<foo>bar</foo>";
+            string _out = @"{""foo"": ""bar"" }";
+            JObject _jOut = JObject.Parse(_out);
+            JObject _jExpected = JObject.Parse(_ws.XmlToJson(_in));
+            Assert.IsTrue(JToken.DeepEquals(_jOut, _jExpected));
+        }
+        public void XmlToJsonTest2()
+        {
+            string _in = @"<TRANS><HPAY><ID>103</ID><STATUS>3</STATUS><EXTRA><IS3DS>0</IS3DS><AUTH>031183</AUTH></EXTRA><INT_MSG/><MLABEL>501767XXXXXX6700</MLABEL><MTOKEN>project01</MTOKEN></HPAY></TRANS>";
+            string _out = @"{""TRANS"": {""HPAY"": {""ID"": ""103"",""STATUS"": ""3"",""EXTRA"": {""IS3DS"": ""0"",""AUTH"": ""031183""},""MLABEL"": ""501767XXXXXX6700"",""MTOKEN"": ""project01""}}}";
+            JObject _jOut = JObject.Parse(_out);
+            JObject _jExpected = JObject.Parse(_ws.XmlToJson(_in));
+            Assert.IsTrue(JToken.DeepEquals(_jOut, _jExpected));
+        }
+        [TestMethod()]
+        public void XmlToJsonFail()
+        {
+            string _in = @"<foo>hello</bar>";
+            Assert.AreEqual("Bad Xml format", _ws.XmlToJson(_in));
+        }
+    }
+}
