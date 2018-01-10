58152cfbc43e0f12a2e1145e424c0fa75dabc85d
samielgtari <35293987+samielgtari@users.noreply.github.com>
2018-01-10 12:58:36 +0100

    Add BLL

---
+++ BLL/BLL/Fibonacci.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using log4net;
+using System.Numerics;
+
+namespace LemonWayTools
+{
+    public static class Fibonacci
+    {
+        /// <summary>
+        /// Gets the fibonacci.
+        /// </summary>
+        /// <param name="n">The n.</param>
+        /// <returns>
+        /// result
+        /// </returns>
+        public static BigInteger GetFibonacci(int n)
+        {
+            
+            if (n >= 1 && n <= 100)
+                return DoFibonacci(n);
+            else
+                return -1;
+        }
+
+        /// <summary>
+        /// Does the fibonacci.
+        /// </summary>
+        /// <param name="n">The n.</param>
+        /// <returns>
+        /// result
+        /// </returns>
+        private static BigInteger DoFibonacci(int n)
+        {
+            BigInteger f1 = 1, res = 1, f2 = 1;
+            for (int i = 2; i < n; i++)
+            {
+                f2 = res + f1;
+                f1 = res;
+                res = f2;
+            }
+            return res;
+        }
+    }
+}
---
+++ BLL/BLL/Formatter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Converters;
+using System.Xml;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace LemonWayTools
+
+{
+    public static class Transformer
+    {
+        /// <summary>
+        /// Transforms the XML.
+        /// </summary>
+        /// <param name="xml">The XML.</param>
+        /// <returns>The Json</returns>
+        public static string TransformXML(string xml)
+        {
+            XmlDocument x = new XmlDocument();
+            try
+            {
+                x.LoadXml(xml);
+            }
+            catch (Exception)
+            {
+                return "Bad Xml format";
+            }
+            return JsonConvert.SerializeXmlNode(x);
+        }
+    }
+}
